b3df5e763c67c836554ccfb71b796ba1a0f51bfc
Andrew Godwin <andrew@aeracode.org>
2022-12-03 14:06:55 -0700

    Switch to secrets module for upload names

--- core/uploads.py
+++ core/uploads.py
@@ -1,6 +1,5 @@
-import base64
 import os
-import uuid
+import secrets
 
 from django.utils import timezone
 
@@ -11,5 +10,5 @@ def upload_namer(prefix, instance, filename):
     """
     now = timezone.now()
     _, old_extension = os.path.splitext(filename)
-    new_filename = base64.b32encode(uuid.uuid4().bytes).decode("ascii")
+    new_filename = secrets.token_urlsafe(20)
     return f"{prefix}/{now.year}/{now.month}/{now.day}/{new_filename}{old_extension}"
