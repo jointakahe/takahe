1f7d6529e03a5bd787120b41e470524bedea596e
Gabriel Rodríguez Alberich <gabriel.rodriguez@jobandtalent.com>
2022-12-15 16:55:05 +0100

    Added pagination to the Notifications page
    We lose the order by the identity name in the way, but that can't
    be efficiently done without some kludgy SQL.

--- activities/views/follows.py
+++ activities/views/follows.py
@@ -1,8 +1,10 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from users.decorators import identity_required
-from users.models import FollowStates
+from users.models import Follow, FollowStates
 
 
 @method_decorator(identity_required, name="dispatch")
@@ -15,19 +17,25 @@ class FollowsPage(TemplateView):
 
     def get_context_data(self):
         # Gather all identities with a following relationship with us
-        identities = {}
-        for outbound_follow in self.request.identity.outbound_follows.filter(
-            state__in=FollowStates.group_active()
-        ):
-            identities.setdefault(outbound_follow.target, {})[
-                "outbound"
-            ] = outbound_follow
-        for inbound_follow in self.request.identity.inbound_follows.filter(
-            state__in=FollowStates.group_active()
-        ):
-            identities.setdefault(inbound_follow.source, {})["inbound"] = inbound_follow
+        follows = Follow.objects.filter(
+            Q(source=self.request.identity) | Q(target=self.request.identity),
+            state__in=FollowStates.group_active(),
+        ).order_by("-id")
+        paginator = Paginator(follows, 50)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        identities = []
+        for follow in page_obj.object_list:
+            if follow.source == self.request.identity:
+                identity = follow.target
+                follow_type = "outbound"
+            else:
+                identity = follow.source
+                follow_type = "inbound"
+            identities.append((identity, follow_type))
+        page_obj.object_list = identities
 
         return {
             "section": "follows",
-            "identities": sorted(identities.items(), key=lambda i: i[0].username),
+            "page_obj": page_obj,
         }
