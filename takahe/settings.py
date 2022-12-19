ecde831e77d79a5e7429e81137f6d12a25831d8b
Andrew Godwin <andrew@aeracode.org>
2022-12-19 00:42:08 +0000

    Sample way less transactions

--- takahe/settings.py
+++ takahe/settings.py
@@ -87,7 +87,7 @@ class Settings(BaseSettings):
     #: An optional Sentry DSN for error reporting.
     SENTRY_DSN: str | None = None
     SENTRY_SAMPLE_RATE: float = 1.0
-    SENTRY_TRACES_SAMPLE_RATE: float = 1.0
+    SENTRY_TRACES_SAMPLE_RATE: float = 0.01
     SENTRY_CAPTURE_MESSAGES: bool = False
 
     #: Fallback domain for links.
