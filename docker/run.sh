4e88cf8c812510b6574849e7b49d25cd47f9df1e
Tyler Kennedy <tk@tkte.ch>
2022-12-21 23:46:20 -0500

    Add a nginx.dev.conf which is used by the dockerfile when django is in debug mode. This fixes issues with static assets not being available unless collectstatic is run even in debug mode.

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,13 @@ CACHE_SIZE="${TAKAHE_NGINX_CACHE_SIZE:-1g}"
 sed -i s/__CACHESIZE__/${CACHE_SIZE}/g /takahe/docker/nginx.conf
 
 # Run nginx and gunicorn
-nginx -c "/takahe/docker/nginx.conf" &
+if [ "$TAKAHE_DEBUG" == "true" ]
+then
+  nginx -c "/takahe/docker/nginx.dev.conf" &
+else
+  nginx -c "/takahe/docker/nginx.conf" &
+fi
+
 gunicorn takahe.wsgi:application -b 0.0.0.0:8001 &
 
 # Wait for any process to exit
