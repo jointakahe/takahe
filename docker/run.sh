7c1d0da5e554e88b8ff74cb897ceb62d5f6abb28
Tyler Kennedy <tk@tkte.ch>
2022-12-22 16:09:05 -0500

    Revert nginx changes.

--- docker/run.sh
+++ docker/run.sh
@@ -5,12 +5,7 @@ CACHE_SIZE="${TAKAHE_NGINX_CACHE_SIZE:-1g}"
 sed -i s/__CACHESIZE__/${CACHE_SIZE}/g /takahe/docker/nginx.conf
 
 # Run nginx and gunicorn
-if [ "$TAKAHE_DEBUG" == "true" ]
-then
-  nginx -c "/takahe/docker/nginx.dev.conf" &
-else
-  nginx -c "/takahe/docker/nginx.conf" &
-fi
+nginx -c "/takahe/docker/nginx.conf" &
 
 gunicorn takahe.wsgi:application -b 0.0.0.0:8001 &
 
