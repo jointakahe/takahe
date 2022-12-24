a80d8b82200e70aeb40743011f787684c15a41bd
Michael Manfre <mike@manfre.net>
2022-12-23 20:39:55 -0500

    Fixes #239 - Add option to hide Boost and Like counts

--- core/models/config.py
+++ core/models/config.py
@@ -243,3 +243,6 @@ class Config(models.Model):
         toot_mode: bool = False
         default_post_visibility: int = 0  # Post.Visibilities.public
         visible_follows: bool = True
+
+        # wellness Options
+        visible_reaction_counts: bool = True
