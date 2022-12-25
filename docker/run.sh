fd99cf9ad8b4e6bf8b930b88da5345d4f1deb685
Corry Haines <github@corryh.com>
2022-12-24 19:21:10 -0800

    Support gunicorn autoreload on DEBUG
    Auto-reload gunicorn whenever source code changes, for somewhat easier development.

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,14 @@
 CACHE_SIZE="${TAKAHE_NGINX_CACHE_SIZE:-1g}"
 sed -i s/__CACHESIZE__/${CACHE_SIZE}/g /takahe/docker/nginx.conf
 
+# Enable auto-reload when in DEBUG mode
+[ "${TAKAHE_DEBUG}" = "true" ] && GUNICORN_RELOAD="--reload"
+
 # Run nginx and gunicorn
 nginx -c "/takahe/docker/nginx.conf" &
 
-gunicorn takahe.wsgi:application -b 0.0.0.0:8001 &
+echo $GUNICORN_RELOAD 4
+gunicorn takahe.wsgi:application -b 0.0.0.0:8001 $GUNICORN_RELOAD &
 
 # Wait for any process to exit
 wait -n
