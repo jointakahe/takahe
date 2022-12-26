2faabd479eebb5e00ab9a828524e2b35b3be9b17
Corry Haines <github@corryh.com>
2022-12-26 13:16:34 -0800

    Introduce debugger friendly docker-compose-split.yml
    To support the new debugger compose definition, tweak run.sh to
    spawn nginx/gunicorn from function calls with kill switches. We
    can then call these kill switches as needed during development mode.
    
    This change has no impact on production or default workflows.

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-# Set up cache size
-CACHE_SIZE="${TAKAHE_NGINX_CACHE_SIZE:-1g}"
-sed s/__CACHESIZE__/${CACHE_SIZE}/g /takahe/docker/nginx.conf > /takahe/docker/nginx.rendered.conf
+function run_gunicorn() {
+  # Provide hook for disable gunicorn in development
+  [ -n "$DISABLE_GUNICORN_RUN" ] && echo "gunicorn disabled" && return
 
-# Run nginx and gunicorn
-nginx -c "/takahe/docker/nginx.rendered.conf" &
+  gunicorn takahe.wsgi:application -b 0.0.0.0:8001 $GUNICORN_EXTRA_CMD_ARGS &
+}
 
-gunicorn takahe.wsgi:application -b 0.0.0.0:8001 $GUNICORN_EXTRA_CMD_ARGS &
+function run_nginx() {
+  # Provide hook for disabling nginx in development
+  [ -n "$DISABLE_NGINX_RUN" ] && echo "nginx disabled" && return
+
+  # Reset the rendered file before rewriting below
+  cp /takahe/docker/nginx.conf /takahe/docker/nginx.rendered.conf
+
+  # Set up cache size
+  CACHE_SIZE="${TAKAHE_NGINX_CACHE_SIZE:-1g}"
+  sed -i s/__CACHESIZE__/${CACHE_SIZE}/g /takahe/docker/nginx.rendered.conf
+
+  # Set the gunicorn host (used primarily for development override)
+  NGINX_GUNICORN_HOST="${NGINX_GUNICORN_HOST:-127.0.0.1}"
+  sed -i s/__GUNICORN_HOST__/${NGINX_GUNICORN_HOST}/g /takahe/docker/nginx.rendered.conf
+
+  # Run nginx and gunicorn
+  nginx -c "/takahe/docker/nginx.rendered.conf" &
+}
+
+run_nginx
+
+run_gunicorn
 
 # Wait for any process to exit
 wait -n
