2788870d49e8c0706d4d0f7f01ad513e36a8ddad
Tyler Kennedy <tk@tkte.ch>
2022-12-29 21:27:38 -0500

    Show a post button and fix selected logic for following/followers

--- users/views/identity.py
+++ users/views/identity.py
@@ -4,6 +4,7 @@ from django import forms
 from django.contrib.auth.decorators import login_required
 from django.contrib.syndication.views import Feed
 from django.core import validators
+from django.db import models
 from django.http import Http404, JsonResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
@@ -74,6 +75,7 @@ class ViewIdentity(ListView):
             context["page_obj"],
             self.request.identity,
         )
+        context["post_count"] = self.identity.posts.count()
         if self.identity.config_identity.visible_follows:
             context["followers_count"] = self.identity.inbound_follows.filter(
                 state__in=FollowStates.group_active()
@@ -164,6 +166,7 @@ class IdentityFollows(ListView):
         context["following_count"] = self.identity.outbound_follows.filter(
             state__in=FollowStates.group_active()
         ).count()
+        context["post_count"] = self.identity.posts.count()
         return context
 
 
