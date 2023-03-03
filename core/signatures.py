26d90a5745fb2b3208eebe22c33f85390c2b5ef8
Kelson Vibber <kelson@pobox.com>
2023-03-02 22:06:34 -0800

    run linter on signature.py change

--- core/signatures.py
+++ core/signatures.py
@@ -162,7 +162,10 @@ class HttpSignature:
         # Reject unknown algorithms
         # hs2019 is used by some libraries to obfuscate the real algorithm per the spec
         # https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12
-        if signature_details["algorithm"] != "rsa-sha256" and signature_details["algorithm"] != "hs2019":
+        if (
+            signature_details["algorithm"] != "rsa-sha256"
+            and signature_details["algorithm"] != "hs2019"
+        ):
             raise VerificationFormatError("Unknown signature algorithm")
         # Create the signature payload
         headers_string = cls.headers_from_request(request, signature_details["headers"])
