eed8ceb966410ab838ac3b9821bd09be87876dee
Dan Watson <dcwatson@gmail.com>
2023-03-09 11:47:03 -0500

    Initial support for IceCubes

--- api/views/apps.py
+++ api/views/apps.py
@@ -1,28 +1,27 @@
 import secrets
 
-from hatchway import Schema, api_view
+from hatchway import QueryOrBody, api_view
 
 from .. import schemas
 from ..models import Application
 
 
-class CreateApplicationSchema(Schema):
-    client_name: str
-    redirect_uris: str
-    scopes: None | str = None
-    website: None | str = None
-
-
 @api_view.post
-def add_app(request, details: CreateApplicationSchema) -> schemas.Application:
+def add_app(
+    request,
+    client_name: QueryOrBody[str],
+    redirect_uris: QueryOrBody[str],
+    scopes: QueryOrBody[None | str] = None,
+    website: QueryOrBody[None | str] = None,
+) -> schemas.Application:
     client_id = "tk-" + secrets.token_urlsafe(16)
     client_secret = secrets.token_urlsafe(40)
     application = Application.objects.create(
-        name=details.client_name,
-        website=details.website,
+        name=client_name,
+        website=website,
         client_id=client_id,
         client_secret=client_secret,
-        redirect_uris=details.redirect_uris,
-        scopes=details.scopes or "read",
+        redirect_uris=redirect_uris,
+        scopes=scopes or "read",
     )
     return schemas.Application.from_orm(application)
